8bc139ea7b660d48bc264dedc1c65c12eac926ae
Shajan Jacob <shajanjp@gmail.com>
2026-04-10 02:43:06 +0530

    fix: node and npm package version change

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,4 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const Image = require("@11ty/eleventy-img");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
-
   // Pass through any static assets if you add them later (e.g., images)
   eleventyConfig.addPassthroughCopy("src/assets");
 
